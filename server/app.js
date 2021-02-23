6012bdb340770a1e6379f47d2efd9c046eb573fe
rivalcoba <ivan.rivalcoba@gmail.com>
2021-02-22 18:43:14 -0600

    modernizanso proyecto Y scripts de arranque

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var createError = require('http-errors');
+import createError from 'http-errors'
+// var express = require('express');
+import express from 'express'
+// var path = require('path');
+import path from 'path'
+// var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser'
+// var logger = require('morgan');
+import logger from 'morgan'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+import indexRouter from './routes/index'
+// var usersRouter = require('./routes/users');
+import usersRouter from './routes/users'
 
 var app = express();
 
